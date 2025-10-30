5a710348b0dc56587e0661268c8cfbb6904c2a49
WeoGZ <weotan@foxmail.com>
2025-10-30 15:25:40 +0800

    初步完成策略翻译，生成信号跟文华对比过，基本一样

--- vectorbt_ysj/strategies/w_s12_v4.py
+++ vectorbt_ysj/strategies/w_s12_v4.py
@@ -15,117 +15,102 @@ import plotly.io as pio
 # 设置渲染器为浏览器
 pio.renderers.default = "browser"
 
-# pd.set_option('Display.max_rows',None)#展示全部行
+# pd.set_option('Display.max_rows',None)  #展示全部行
 pd.set_option('Display.max_columns', None)  # 展示全部列
 
 
-def execute(symbol: str, start_date: datetime, end_date: datetime, interval: Interval, len: int, stpr: int, n: int):
+def execute(symbol: str, start_date: datetime, end_date: datetime, interval: Interval, length: int, stpr: int, n: int):
     """程序入口"""
     _, klines_high, klines_low, klines_close, klines_vol = fetch_klines([symbol], start_date, end_date, interval)
     if klines_close is not None and not klines_close.empty:
-        pass
+        print(f'>>total_len={len(klines_close)}')
+        calculate_signals(klines_close[symbol], klines_high[symbol], klines_low[symbol], klines_vol[symbol],
+                          interval, length, stpr, n)
 
 
 def calculate_signals(close_price: pd.Series, high_price: pd.Series, low_price: pd.Series, vols: pd.Series,
                       interval: Interval, length: int, stpr: int, n: int):
     """计算交易信号"""
     if close_price is not None and not close_price.empty:
-        in_trade_list = np.zeros(close_price.size)
+        dt_list = pd.Series(close_price.index)
 
         cnn: int = cal_kline_len_single_day(close_price, interval)
+        print(f'>>cnn={cnn}')
         zd: pd.Series = cal_zd(close_price, n, cnn)
+        # print(f'>>zd[-10:]={zd[-10:]}')
+        print(f'>>转震荡信号点={dt_list[np.where((zd == 1) & (MyTT.REF(zd, 1) == 0))[0]]}')
+        print(f'>>结束震荡信号点={dt_list[np.where((zd == 0) & (MyTT.REF(zd, 1) == 1))[0]]}')
         cd = close_price - MyTT.REF(close_price, 1)
         buy_v = MyTT.SUM(MyTT.IF(cd > 0, vols, 0), length)
         sell_v = MyTT.SUM(MyTT.IF(cd < 0, vols, 0), length)
         bsr = buy_v / sell_v
         last_bar_hour = LAST_BAR_START_TIME_MAP[interval][0]
         last_bar_minute = LAST_BAR_START_TIME_MAP[interval][1]
-        bsl = pd.Series([bsr[i - 1] if i > 0 and close_price[i - 1].index.hour == last_bar_hour and
-                                       close_price[i - 1].index.minute == last_bar_minute else np.nan
-                         for i in range(close_price.size)])
-        for i in range(1, bsl.size):
+        bsl = np.array([bsr[i - 1] if i > 0 and close_price.index[i - 1].hour == last_bar_hour and
+                                      close_price.index[i - 1].minute == last_bar_minute else np.nan
+                        for i in range(close_price.size)])
+        for i in range(1, len(bsl)):
             if np.isnan(bsl[i]) and not np.isnan(bsl[i - 1]):
                 bsl[i] = bsl[i - 1]
         mbl = np.maximum(bsl, 1)
         msl = np.minimum(bsl, 1)
 
-        crossup_bsr_mbl = MyTT.CROSS(bsr, mbl) and MyTT.REF(in_trade_list, 1) == 0
+        crossup_bsr_mbl = MyTT.CROSS(bsr, mbl)
         op_l = MyTT.BARSLAST(crossup_bsr_mbl)
-        crossdown_bsr_msl = MyTT.CROSS(msl, bsr) and MyTT.REF(in_trade_list, 1) == 0
+        crossdown_bsr_msl = MyTT.CROSS(msl, bsr)
         op_s = MyTT.BARSLAST(crossdown_bsr_msl)
-        op_temp1 = ~np.isnan(op_l) and ~np.isnan(op_s)
-        op = MyTT.IF(op_temp1, np.minimum(op_l, op_s), MyTT.IF(np.isnan(op_l), op_s, MyTT.IF(np.isnan(op_s), op_l, -1)))
-        dk = MyTT.IF(op_l == 0, 1, MyTT.IF(op_s == 0, -1, np.nan))
-        # dk特殊处理：当等于nan时取前一个有效值
-        for i in range(1, dk.size):
-            if np.isnan(dk[i]) and not np.isnan(dk[i - 1]):
-                dk[i] = dk[i - 1]
 
         # 开仓信号
-        bkcon = op_l == 0 and zd == 0  # 多头开仓
-        skcon = op_s == 0 and zd == 0  # 空头开仓
+        bkcon = [True if i == 0 and j == 0 else False for i, j in zip(op_l, zd)]  # 多头开仓
+        skcon = [True if i == 0 and j == 0 else False for i, j in zip(op_s, zd)]  # 空头开仓
 
         # 平仓信号——止盈止损
         stbar = 40
-        jp = MyTT.IF(MyTT.REF(dk, 1) == 1, MyTT.REF(MyTT_plus.HHV(low_price.values, op), 1),
-                     MyTT.IF(MyTT.REF(dk, 1) == -1, MyTT.REF(MyTT_plus.LLV(high_price.values, op), 1), np.nan))
-        vp = jp * MyTT.IF(MyTT.REF(dk, 1) == 1, 1 - stbar / 1000, MyTT.IF(MyTT.REF(dk, 1) == -1, 1 + stbar / 1000, np.nan))
-        cn = MyTT.IF(np.isnan(dk), np.nan, op)
-        sum_vol = MyTT_plus.SUM(vols.values, cn) if not np.isnan(cn) else np.nan
-        sum_amt = MyTT_plus.SUM(vols.values * vp, cn) if not np.isnan(cn) else np.nan
-        vwap = MyTT.IF(op > 0, MyTT.IF(op >= stbar, vp, sum_amt / sum_vol), np.nan)
-        pcon_l = MyTT.REF(dk, 1) == 1 and low_price <= vwap
-        pcon_s = MyTT.REF(dk, 1) == -1 and high_price >= vwap
 
-        # 平仓信号——转震荡
-        zd_close_l = MyTT.REF(zd, 1) == 0 and zd == 1 and MyTT.REF(dk, 1) == 1
-        zd_close_s = MyTT.REF(zd, 1) == 0 and zd == 1 and MyTT.REF(dk, 1) == -1
+        # 特别处理：未触发出场条件前，过滤掉重复开仓信号
+        dk_l = MyTT.IF(bkcon, 1, MyTT.IF(skcon, 2, 0))  # 多空交叉信号
+        pre_final_close_index = np.nan
+        finish = False
+        while not finish:
+            dk_l = handle_close_operation(dk_l, vols, low_price, high_price, zd, stpr, stbar)
+            close_indexes = np.where((dk_l == -1) | (dk_l == -2))[0]
+            final_close_index = close_indexes[-1] if len(close_indexes) > 0 else np.nan  # 最后一个平仓信号的下标
+            dt_pre_final = np.nan if np.isnan(pre_final_close_index) else dt_list[pre_final_close_index]
+            print(f'>>pre_final_close_index={pre_final_close_index}({dt_pre_final}), \
+                    final_close_index={final_close_index}({dt_list[final_close_index]})')
+            finish = (final_close_index == pre_final_close_index) or (
+                    np.isnan(final_close_index) and np.isnan(pre_final_close_index))
+            pre_final_close_index = final_close_index
+        # print(f'>>dk_l[-50:]={dk_l[-50:]}')
+        print(f'>>多开信号={dt_list[np.where(dk_l == 1)[0]]}')
+        print(f'>>多平信号={dt_list[np.where(dk_l == -1)[0]]}')
+        print(f'>>空开信号={dt_list[np.where(dk_l == 2)[0]]}')
+        print(f'>>空平信号={dt_list[np.where(dk_l == -2)[0]]}')
 
-        # 特别处理：未平仓时不反向开仓
-        # # 多单未平、不开空单
-        # bkcon2 = pd.Series(
-        #     [True if pcon_l[i] and skcon[i] and low_price[i] > vwap[i] else bkcon[i] for i in range(bkcon)])
-        # pcon_l2 = pd.Series(
-        #     [False if pcon_l[i] and skcon[i] and low_price[i] > vwap[i] else pcon_l[i] for i in range(bkcon)])
-        # skcon2 = pd.Series(
-        #     [False if pcon_l[i] and skcon[i] and low_price[i] > vwap[i] else skcon[i] for i in range(bkcon)])
-        # # 空单未平、不开多单
-        # skcon2 = pd.Series(
-        #     [True if pcon_s[i] and bkcon[i] and high_price[i] < vwap[i] else skcon[i] for i in range(bkcon)])
-        # pcon_s2 = pd.Series(
-        #     [False if pcon_s[i] and bkcon[i] and high_price[i] < vwap[i] else pcon_s[i] for i in range(bkcon)])
-        # bkcon2 = pd.Series(
-        #     [False if pcon_s[i] and bkcon[i] and high_price[i] < vwap[i] else skcon[i] for i in range(bkcon)])
 
-        bkcon2 = pd.Series(bkcon)
-        skcon2 = pd.Series(skcon)
-        pcon_l2 = pd.Series(pcon_l)
-        pcon_s2 = pd.Series(pcon_s)
-        in_trade = False
-        for i in range(close_price.size):
-            if ~in_trade and (bkcon[i] or skcon[i]):
-                in_trade = True
-
-            if in_trade:
-                # 多单未平、不开空单
-                if pcon_l[i] and skcon[i]:
-                    if low_price[i] > vwap[i]:
-                        bkcon2[i] = True
-                        pcon_l2[i] = False
-                        skcon2[i] = False
-                    else:  # 若同一根K线同时触发多单止盈止损和开空，则忽略开空
-                        skcon2[i] = False
-
-                    dk[i] = 1
-                elif pcon_s[i] and bkcon[i]:
-                    if high_price[i] < vwap[i]:
-                        skcon2[i] = True
-                        pcon_s2[i] = False
-                        bkcon2[i] = False
-                    else:  # 若同一根K线同时触发空单止盈止损和开多，则忽略开多
-                        bkcon2[i] = False
-
-                    dk[i] = -1
+def handle_close_operation(dk_l: np.ndarray, vols: pd.Series, low_price: pd.Series, high_price: pd.Series,
+                           zd: pd.Series, stpr: int, stbar: int):
+    """更新出场信号（多单的平仓信号是-1、空单是-2），以及将平仓信号前的重复开仓信号置为0。注意：此函数每次只处理一个多/空信号。"""
+    cumsum_dk_l = MyTT.SUM(dk_l, 0)
+    cn_l = MyTT.IF(cumsum_dk_l > 0, MyTT.BARSLAST(MyTT.CROSS(cumsum_dk_l, 0)), np.nan)  # 最近一次交叉信号的位置
+    signal = MyTT_plus.REF(dk_l, cn_l)  # 多空信号。1表示多头，2表示空头
+    jp_temp = MyTT.IF(signal == 1, MyTT_plus.HV(low_price.values, cn_l),
+                      MyTT.IF(signal == 2, MyTT_plus.LV(high_price.values, cn_l), np.nan))
+    jp_l = MyTT.IF(MyTT.REF(cumsum_dk_l, 1) > 0, jp_temp, np.nan)
+    vp_l = MyTT.IF(signal == 1, jp_l * (1 - stpr / 1000),
+                   MyTT.IF(signal == 2, jp_l * (1 + stpr / 1000), np.nan))
+    sum_vol_l = MyTT.IF(cn_l > 0, MyTT_plus.SUM(vols.values, cn_l), np.nan)
+    sum_amt_l = MyTT.IF(cn_l > 0, MyTT_plus.SUM(vols.values * vp_l, cn_l), np.nan)
+    vwap_l = MyTT.IF(cn_l > 0, MyTT.IF(cn_l >= stbar, vp_l, sum_amt_l / sum_vol_l), np.nan)
+    close_con = MyTT.IF(signal == 1, low_price.values <= vwap_l,
+                        MyTT.IF(signal == 2, high_price.values >= vwap_l, np.nan))
+    cnt_close = MyTT_plus.SUM(close_con, cn_l)  # 开仓后触发出场条件的次数
+    con1 = (zd == 1) & (MyTT.REF(zd, 1) == 0) & (cnt_close == 0)  # 转震荡（首次触发出场条件前）
+    con2 = (close_con == 1) & (cnt_close == 1) & (MyTT_plus.SUM(con1, cn_l) == 0)  # 首次触发出场条件（之前没有转震荡信号）
+    con = con1 | con2
+    dk_l_2 = MyTT.IF(cn_l > 0, MyTT.IF(con, MyTT.IF(signal == 1, -1, -2), MyTT.IF(cnt_close == 0, 0, dk_l)),
+                     dk_l)  # 更新出场信号，以及将平仓信号前的重复开仓信号置为0
+    return dk_l_2
 
 
 def cal_kline_len_single_day(prices: pd.Series, interval: Interval):
@@ -138,7 +123,7 @@ def cal_kline_len_single_day(prices: pd.Series, interval: Interval):
         start_minute = 0
         # 判断是否有夜盘K线
         night_bars_count = prices[prices.index.hour >= 21].size
-        day_minutes: list[int] = [bar.index.minute for bar in prices if bar.index.hour == 9]
+        day_minutes: list[int] = [index.minute for index, value in prices.items() if index.hour == 9]
         if night_bars_count > 0:
             start_hour = 21
         elif len(day_minutes) > 0:
@@ -149,11 +134,12 @@ def cal_kline_len_single_day(prices: pd.Series, interval: Interval):
         kline_len_per_day: list[int] = []
         start_index = -1
         end_index = -1
-        for i in reversed(prices.index):
-            if i.hour == start_time_array[0] and i.minute == start_time_array[1] and i.second == 0:
+        for i in range(len(prices) - 1, -1, -1):
+            if (prices.index[i].hour == start_time_array[0] and prices.index[i].minute == start_time_array[1] and
+                    prices.index[i].second == 0):
                 end_index = i
-            elif (prices[i].index.hour == start_hour and prices[i].index.minute == start_minute and
-                  prices[i].index.second == 0 and end_index != -1):
+            elif (prices.index[i].hour == start_hour and prices.index[i].minute == start_minute and
+                  prices.index[i].second == 0 and end_index != -1):
                 start_index = i
             if start_index != -1 and end_index != -1:
                 already_cal_day += 1
@@ -171,15 +157,15 @@ def cal_zd(prices: pd.Series, n: int, cnn: int):
     zf: pd.Series = MyTT.LN(prices / MyTT.REF(prices, 1)) * 100
     vlt = MyTT.STD(zf, cnn * n) * MyTT.SQRT(252 * cnn)
     ma_vlt = MyTT.MA(vlt, math.floor(n / 3) * cnn)
-    ln1 = max(3, math.floor(n / 2))
+    ln1 = max(3, math.floor(cnn / 2))
 
     # 波动率波峰/波谷拐点
     bf_right = MyTT.REF(ma_vlt, ln1) > MyTT.HHV(ma_vlt, ln1)  # 波峰结构的右边
     bg_right = MyTT.REF(ma_vlt, ln1) < MyTT.LLV(ma_vlt, ln1)  # 波谷结构的右边
-    bf_left = MyTT.REF(ma_vlt >= MyTT.REF(MyTT.HHV(ma_vlt, ln1), 1), ln1).fillna(False).convert_dtypes()
-    bf_left2 = MyTT.REF(ma_vlt >= MyTT.REF(MyTT.HHV(ma_vlt, 20 * cnn), 1), ln1).fillna(False).convert_dtypes()
-    bg_left = MyTT.REF(ma_vlt <= MyTT.REF(MyTT.LLV(ma_vlt, ln1), 1), ln1).fillna(False).convert_dtypes()
-    bg_left2 = MyTT.REF(ma_vlt <= MyTT.REF(MyTT.LLV(ma_vlt, 20 * cnn), 1), ln1).fillna(False).convert_dtypes()
+    bf_left = pd.Series(MyTT.REF(ma_vlt >= MyTT_plus.HV(ma_vlt, ln1), ln1))
+    bf_left2 = pd.Series(MyTT.REF(ma_vlt >= MyTT_plus.HV(ma_vlt, 20 * cnn), ln1))
+    bg_left = pd.Series(MyTT.REF(ma_vlt <= MyTT_plus.LV(ma_vlt, ln1), ln1))
+    bg_left2 = pd.Series(MyTT.REF(ma_vlt <= MyTT_plus.LV(ma_vlt, 20 * cnn), ln1))
     turn_tag = MyTT.IF(bf_right & bf_left & bf_left2, 1, MyTT.IF(bg_right & bg_left & bg_left2, -1, np.nan))
 
     hvlt_temp = MyTT.BARSLAST(turn_tag == 1)
@@ -205,7 +191,8 @@ if __name__ == "__main__":
     t0 = datetime.now()
 
     startDate = datetime(2024, 1, 1, 9, 0, 0)
-    endDate = datetime(2025, 5, 14, 15, 0, 0)
+    endDate = datetime(2025, 4, 23, 15, 0, 0)
+    execute('RBL9', startDate, endDate, Interval.MINUTE60, 250, 20, 70)
 
     t1 = datetime.now()
     print(f'\n>>>>>>总耗时{t1 - t0}s')
